4285d56343facd1d0e06eeb8348e8c7ffcef483e
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-11-07 15:04:34 +0800

    Update EditQuestionContainer.js

--- client/src/pages/QuestionBank/EditQuestionContainer.js
+++ client/src/pages/QuestionBank/EditQuestionContainer.js
@@ -6,32 +6,61 @@ import CustomMidContainer from "../../components/GridComponents/CustomMidContain
 import CustomColumn from "../../components/GridComponents/CustomColumn";
 import Title from "../../components/Title";
 import "../../css/general.css";
+import CustomLabelTitle from "../../components/ProfileComponents/CustomLabelTitle";
+import TextArea from "../../components/TextArea";
 
 class EditQuestionContainer extends Component {
-    render() {
-        console.log("rendered");
-        return (
-          <>
-            <Header />
-            <CustomFullContainer>
-              <CustomMidContainer style={[styles.customMidContainer]}>
+  constructor() {
+    super();
+    this.state = {
+      questionDescriptive: null,
+    };
+  }
+
+  onChangeDescription = (e) => {
+    this.setState({ questionDescriptive: e.target.value });
+  };
+
+  render() {
+    const { questionDescriptive } = this.state;
+
+    console.log("rendered");
+    return (
+      <>
+        <Header />
+        <CustomFullContainer>
+          <CustomMidContainer style={[styles.customMidContainer]}>
+            <CustomColumn>
+              <Title>Edit Question</Title>
+              <form onSubmit={this.onSubmit}>
                 <CustomColumn>
-                  <Title>Edit Question</Title>
-                  
+                  <CustomLabelTitle>Question Description</CustomLabelTitle>
+                  <div style={{ paddingBottom: "25px" }}>
+                    <TextArea
+                      name={"description"}
+                      type={"text"}
+                      placeholder={"Enter the description here"}
+                      onChange={this.onChangeDescription}
+                      value={questionDescriptive}
+                    />
+                  </div>
+
+                  {/* <input type="submit" value="Submit" /> */}
                 </CustomColumn>
-              </CustomMidContainer>
-            </CustomFullContainer>
-          </>
-        );
-      }
+              </form>
+            </CustomColumn>
+          </CustomMidContainer>
+        </CustomFullContainer>
+      </>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-    customMidContainer: {
-      paddingLeft: "10px",
-      paddingBottom: "75px",
-    },
-  });
-  
-  export default EditQuestionContainer;
+  customMidContainer: {
+    paddingLeft: "10px",
+    paddingBottom: "75px",
+  },
+});
+
+export default EditQuestionContainer;
