c046bd26a48661c8ea88cd2e3ead4d9d44feda01
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-11-10 22:30:20 +0800

    statistics and charts setup
    partially finish, grasp the way the library works, need actual data to work, UI will be set based on the results

--- client/src/pages/Assessment/StatisticsContainer.js
+++ client/src/pages/Assessment/StatisticsContainer.js
@@ -7,6 +7,26 @@ import CustomColumn from "../../components/GridComponents/CustomColumn";
 import Title from "../../components/Title";
 import "../../css/general.css";
 
+import * as configStyles from "../../config/styles";
+import { Pie } from "react-chartjs-2";
+
+const data = {
+  labels: ["Pass", "Fail"],
+  datasets: [
+    {
+      //   label: "My First dataset",
+      //   backgroundColor: configStyles.colors.lightBlue,
+      backgroundColor: ["green", "red"],
+
+      //   borderColor: "rgba(255,99,132,1)",
+      borderWidth: 1,
+      hoverBackgroundColor: "rgba(255,99,132,0.4)",
+      hoverBorderColor: "rgba(255,99,132,1)",
+      data: ["40", "60"],
+    },
+  ],
+};
+
 export default class StatisticsContainer extends Component {
   render() {
     return (
@@ -16,8 +36,16 @@ export default class StatisticsContainer extends Component {
           <CustomMidContainer style={[styles.customMidContainer]}>
             <CustomColumn>
               <Title>Statistics</Title>
-
-             
+              <div>
+                <Pie
+                  data={data}
+                  width={300}
+                  height={520}
+                  options={{
+                    maintainAspectRatio: false,
+                  }}
+                />
+              </div>
             </CustomColumn>
           </CustomMidContainer>
         </CustomFullContainer>
@@ -30,5 +58,6 @@ const styles = StyleSheet.create({
   customMidContainer: {
     paddingLeft: "10px",
   },
-
 });
+
+//https://stackoverflow.com/questions/46420578/it-is-possible-to-change-the-color-of-periphery-of-pie-chart-in-chart-js
