fcb41c9f8273f57f8c527eff6f989d005c65b0ae
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-11-18 01:29:03 +0800

    uodate

--- client/src/pages/Assessment/CreateAssessmentContainer.js
+++ client/src/pages/Assessment/CreateAssessmentContainer.js
@@ -6,13 +6,27 @@ import CustomMidContainer from "../../components/GridComponents/CustomMidContain
 import CustomColumn from "../../components/GridComponents/CustomColumn";
 import FirstLabel from "../../components/LabelComponent/FirstLabel";
 import "../../css/general.css";
+import CustomInput from "../../components/CustomInput";
+import SecondLabel from "../../components/LabelComponent/SecondLabel";
+import TextArea from "../../components/TextArea";
 
 // import * as configStyles from "../../config/styles";
 
-
-
 export default class CreateAssessmentContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      testName: "",
+      testDescription: "",
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { testName, testDescription } = this.state;
     return (
       <>
         <Header />
@@ -22,7 +36,26 @@ export default class CreateAssessmentContainer extends Component {
               <div style={{ paddingTop: "60px" }}>
                 <FirstLabel>Create Assessment</FirstLabel>
               </div>
-              
+              <SecondLabel>Assessment Name</SecondLabel>
+              <div style={{ paddingBottom: "25px" }}>
+                <CustomInput
+                  name={"testName"}
+                  type={"text"}
+                  placeholder={" Enter your assessment name"}
+                  onChangeValue={this.onChange}
+                  value={testName}
+                />
+              </div>
+              <SecondLabel>Assessment Description</SecondLabel>
+              <div style={{ paddingBottom: "25px" }}>
+                <TextArea
+                  name={"testDescription"}
+                  type={"text"}
+                  placeholder={"Enter the description here"}
+                  onChange={this.onChange}
+                  value={testDescription}
+                />
+              </div>
             </CustomColumn>
           </CustomMidContainer>
         </CustomFullContainer>
