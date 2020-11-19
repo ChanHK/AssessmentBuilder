5e261c260c7f82669efc7fd181dcefb26fc0a742
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-11-19 16:16:27 +0800

    Create index.js
    - new component for short answer, completed

---
+++ client/src/components/ShortAns/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { css, StyleSheet } from "aphrodite";
+import TextArea from "../../components/TextArea";
+import CustomRow from "../../components/GridComponents/CustomRow";
+import * as MdIcons from "react-icons/md";
+import * as configStyles from "../../config/styles";
+
+const ShortAns = (props) => (
+  <div className={css(styles.con)}>
+    <CustomRow>
+      <div style={{ width: "90%" }}>
+        <TextArea
+          name={props.name}
+          type={"text"}
+          placeholder={props.placeholder}
+          onChange={props.onChange}
+          value={props.value}
+          height={props.height}
+        />
+      </div>
+      <div style={{ width: "10%" }} className={css(styles.button)}>
+        <MdIcons.MdDelete size={45} onClick={props.onClick} />
+      </div>
+    </CustomRow>
+  </div>
+);
+
+const styles = StyleSheet.create({
+  con: {
+    width: "100%",
+    paddingBottom: "25px",
+  },
+  button: {
+    color: configStyles.colors.darkBlue,
+    cursor: "pointer",
+    height: "50px",
+    ":active": {
+      color: configStyles.colors.red,
+      size: 40,
+    },
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+  },
+});
+
+export default ShortAns;
