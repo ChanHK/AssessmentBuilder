ce03912a292b1cd7f9b16a6aac349d825471a7c5
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-11-21 21:57:54 +0800

    Update ViewResponseContainer.js

--- client/src/pages/Assessment/ViewResponseContainer.js
+++ client/src/pages/Assessment/ViewResponseContainer.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import { StyleSheet } from "aphrodite";
+import { StyleSheet, css } from "aphrodite";
 import Header from "../../components/Header";
 import CustomFullContainer from "../../components/GridComponents/CustomFullContainer";
 import CustomMidContainer from "../../components/GridComponents/CustomMidContainer";
 import CustomColumn from "../../components/GridComponents/CustomColumn";
 import FirstLabel from "../../components/LabelComponent/FirstLabel";
 import "../../css/general.css";
+import CustomRow from "../../components/GridComponents/CustomRow";
+import SecondLabel from "../../components/LabelComponent/SecondLabel";
+import ThirdLabel from "../../components/LabelComponent/ThirdLabel";
 
 // import * as configStyles from "../../config/styles";
 
-// this is for viewing candidates responses 
+// this is for viewing candidates responses
 
 export default class ViewResponseContainer extends Component {
   render() {
@@ -22,7 +25,37 @@ export default class ViewResponseContainer extends Component {
               <div style={{ paddingTop: "60px" }}>
                 <FirstLabel>View Response</FirstLabel>
               </div>
-              
+
+              <CustomRow>
+                <div className={css(styles.infoCon)}>
+                  <SecondLabel marginRight={"10px"}>
+                    Candidate Name :{" "}
+                  </SecondLabel>
+                  <ThirdLabel>Captain Jack Sparrow</ThirdLabel>
+                </div>
+              </CustomRow>
+              <CustomRow>
+                <div className={css(styles.infoCon)}>
+                  <SecondLabel marginRight={"10px"}>
+                    Candidate Email :{" "}
+                  </SecondLabel>
+                  <ThirdLabel>CaptainJack2020@gmail.com</ThirdLabel>
+                </div>
+              </CustomRow>
+              <CustomRow>
+                <div className={css(styles.infoCon)}>
+                  <SecondLabel marginRight={"10px"}>
+                    Submission Date :{" "}
+                  </SecondLabel>
+                  <ThirdLabel>2020-09-13 23:07</ThirdLabel>
+                </div>
+              </CustomRow>
+              <CustomRow>
+                <div className={css(styles.infoCon)}>
+                  <SecondLabel marginRight={"10px"}>Score : </SecondLabel>
+                  <ThirdLabel>100</ThirdLabel>
+                </div>
+              </CustomRow>
             </CustomColumn>
           </CustomMidContainer>
         </CustomFullContainer>
@@ -34,5 +67,12 @@ export default class ViewResponseContainer extends Component {
 const styles = StyleSheet.create({
   customMidContainer: {
     paddingLeft: "10px",
+  },
+  infoCon: {
+    width: "100%",
+    height: "auto",
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "10px",
   },
 });
