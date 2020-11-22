382624685aefeefab7ed7fed9358a38e26d070fb
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-11-22 21:22:25 +0800

    - qr
    - add qr comp
    - add click copy comp

--- client/src/pages/Assessment/AccessContainer.js
+++ client/src/pages/Assessment/AccessContainer.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
-import { StyleSheet } from "aphrodite";
+import { StyleSheet, css } from "aphrodite";
 import Header from "../../components/Header";
 import CustomFullContainer from "../../components/GridComponents/CustomFullContainer";
 import CustomMidContainer from "../../components/GridComponents/CustomMidContainer";
 import CustomColumn from "../../components/GridComponents/CustomColumn";
 import FirstLabel from "../../components/LabelComponent/FirstLabel";
 import "../../css/general.css";
-// import SecondLabel from "../../components/LabelComponent/SecondLabel";
+import SecondLabel from "../../components/LabelComponent/SecondLabel";
 // import ThirdLabel from "../../components/LabelComponent/ThirdLabel";
 import AccessButtonBar from "../../components/AccessButtonBar";
+import * as configStyles from "../../config/styles";
+import QRCode from "qrcode.react";
+import ClickCopy from "../../components/ClickCopy";
 
 class AccessContainer extends Component {
   constructor() {
     super();
     this.state = {
       accessType: "",
+      link: "http://abc/abc/abc.com",
+      fullPageQR: false,
     };
   }
 
@@ -23,17 +28,65 @@ class AccessContainer extends Component {
   };
 
   render() {
-    const { accessType } = this.state;
+    const { accessType, link, fullPageQR } = this.state;
+    console.log(fullPageQR);
     return (
       <>
         <Header />
         <CustomFullContainer>
           <CustomMidContainer style={[styles.customMidContainer]}>
             <CustomColumn>
-              <div style={{ paddingTop: "60px" }}>
-                <FirstLabel>Access</FirstLabel>
-              </div>
-              <AccessButtonBar onClick={this.onGetType} type={accessType} />
+              {fullPageQR ? (
+                <>
+                  <div className={css(styles.modal)}>
+                    <div className={css(styles.modalContent)}>
+                      <QRCode
+                        id="123456"
+                        value={link} // the link
+                        level={"H"}
+                        includeMargin
+                        className={css(styles.qr)}
+                        onClick={() =>
+                          this.setState({ fullPageQR: !fullPageQR })
+                        }
+                        style={{ width: "50%" }}
+                      />
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div style={{ paddingTop: "60px" }}>
+                    <FirstLabel>Access</FirstLabel>
+                  </div>
+                  <AccessButtonBar onClick={this.onGetType} type={accessType} />
+
+                  <div className={css(styles.linkCon)}>
+                    <SecondLabel>Link</SecondLabel>
+                    <ClickCopy
+                      value={link}
+                      onClick={() => {
+                        navigator.clipboard.writeText(link);
+                      }}
+                    />
+                    <div style={{ padding: "25px 0px" }}>
+                      <SecondLabel>QR Code</SecondLabel>
+
+                      <QRCode
+                        id="123456"
+                        value={link} // the link
+                        level={"H"}
+                        includeMargin
+                        className={css(styles.qr)}
+                        onClick={() =>
+                          this.setState({ fullPageQR: !fullPageQR })
+                        }
+                        style={{ width: "40%" }}
+                      />
+                    </div>
+                  </div>
+                </>
+              )}
             </CustomColumn>
           </CustomMidContainer>
         </CustomFullContainer>
@@ -45,6 +98,44 @@ class AccessContainer extends Component {
 const styles = StyleSheet.create({
   customMidContainer: {
     paddingLeft: "10px",
+  },
+  linkCon: {
+    backgroundColor: configStyles.colors.lightGrey,
+    width: "100%",
+    marginTop: "50px",
+    borderRadius: "5px",
+    borderColor: configStyles.colors.black,
+    border: "2px solid",
+    padding: 20,
+    marginBottom: "50px",
+  },
+  qr: {
+    borderRadius: "5px",
+    border: "2px solid",
+    borderColor: configStyles.colors.black,
+    height: "auto",
+    display: "flex",
+    cursor: "pointer",
+  },
+  modal: {
+    display: "flex",
+    position: "fixed",
+    zIndex: -3,
+    left: 0,
+    top: 0,
+    width: "100%",
+    height: "100vh",
+    backgroundColor: "rgb(0, 0, 0)",
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    width: "80%",
+    height: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
   },
 });
 
