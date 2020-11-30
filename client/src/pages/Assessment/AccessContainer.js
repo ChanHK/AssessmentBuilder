15671d65d5245030b2a3b094956f31060705602c
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-11-30 23:45:36 +0800

    Update AccessContainer.js

--- client/src/pages/Assessment/AccessContainer.js
+++ client/src/pages/Assessment/AccessContainer.js
@@ -1,31 +1,32 @@
 import React, { Component } from "react";
 import { StyleSheet, css } from "aphrodite";
 import CustomColumn from "../../components/GridComponents/CustomColumn";
+import CustomRow from "../../components/GridComponents/CustomRow";
+
 import "../../css/general.css";
 import SecondLabel from "../../components/LabelComponent/SecondLabel";
-// import ThirdLabel from "../../components/LabelComponent/ThirdLabel";
-import AccessButtonBar from "../../components/AccessButtonBar";
+import ThirdLabel from "../../components/LabelComponent/ThirdLabel";
+
 import * as configStyles from "../../config/styles";
 import QRCode from "qrcode.react";
 import ClickCopy from "../../components/ClickCopy";
 
 import Modal from "../../components/Modal";
+import Radio from "../../components/Radio";
+import Button from "../../components/Button";
 
 class AccessContainer extends Component {
   constructor() {
     super();
     this.state = {
-      accessType: "",
       link: "http://abc/abc/abc.com",
 
       showModal: false,
+      noAuthenticationSelected: false,
+      privateAuthenticationSelected: false,
+      groupAuthenticationSelected: false,
     };
   }
-
-  onGetType = (e) => {
-    console.log("a");
-    this.setState({ accessType: e.target.value });
-  };
 
   modalHandler = () => {
     this.setState({
@@ -33,8 +34,38 @@ class AccessContainer extends Component {
     });
   };
 
+  noAuthenticationOnClick = (e) => {
+    this.setState({
+      noAuthenticationSelected: e.target.checked,
+      privateAuthenticationSelected: false,
+      groupAuthenticationSelected: false,
+    });
+  };
+
+  privateAuthenticationOnClick = (e) => {
+    this.setState({
+      privateAuthenticationSelected: e.target.checked,
+      noAuthenticationSelected: false,
+      groupAuthenticationSelected: false,
+    });
+  };
+
+  groupAuthenticationOnClick = (e) => {
+    this.setState({
+      groupAuthenticationSelected: e.target.checked,
+      noAuthenticationSelected: false,
+      privateAuthenticationSelected: false,
+    });
+  };
+
   render() {
-    const { accessType, link, showModal } = this.state;
+    const {
+      link,
+      showModal,
+      noAuthenticationSelected,
+      privateAuthenticationSelected,
+      groupAuthenticationSelected,
+    } = this.state;
     return (
       <form>
         <CustomColumn>
@@ -70,8 +101,52 @@ class AccessContainer extends Component {
 
           <SecondLabel>Authentication Methods</SecondLabel>
 
-          <AccessButtonBar onClick={this.onGetType} type={accessType} />
-          <div style={{ paddingBottom: "500px" }}></div>
+          <div className={css(styles.bar)}>
+            <CustomRow>
+              <div className={css(styles.radionCon)}>
+                <div style={{ paddingRight: "20px" }}>
+                  <Radio
+                    checked={noAuthenticationSelected}
+                    onChange={this.noAuthenticationOnClick}
+                  />
+                </div>
+                <ThirdLabel>No authentication</ThirdLabel>
+              </div>
+            </CustomRow>
+            <CustomRow>
+              <div className={css(styles.radionCon)}>
+                <div style={{ paddingRight: "20px" }}>
+                  <Radio
+                    checked={privateAuthenticationSelected}
+                    onChange={this.privateAuthenticationOnClick}
+                  />
+                </div>
+                <ThirdLabel>Private access code</ThirdLabel>
+              </div>
+            </CustomRow>
+            <CustomRow>
+              <div className={css(styles.radionCon)}>
+                <div style={{ paddingRight: "20px" }}>
+                  <Radio
+                    checked={groupAuthenticationSelected}
+                    onChange={this.groupAuthenticationOnClick}
+                  />
+                </div>
+                <ThirdLabel>Group access code</ThirdLabel>
+              </div>
+            </CustomRow>
+          </div>
+          <div style={{ paddingBottom: "300px" }}>
+            <Button
+              backgroundColor={configStyles.colors.darkBlue}
+              color={configStyles.colors.white}
+              padding={"8px"}
+              width={"100px"}
+              type={"submit"}
+            >
+              Save
+            </Button>
+          </div>
         </CustomColumn>
       </form>
     );
@@ -95,6 +170,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     display: "flex",
+  },
+  bar: {
+    width: "100%",
+    padding: "20px 20px",
+    marginBottom: "50px",
+    border: "2px solid",
+    borderRadius: "5px",
+    borderColor: configStyles.colors.black,
+  },
+  radionCon: {
+    width: "100%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    display: "flex",
+    marginBottom: "20px",
   },
 });
 
