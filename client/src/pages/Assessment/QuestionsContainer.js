fbd6764fb4d2951da9153011896f6e0013bd39fc
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-12-05 23:47:40 +0800

    Update QuestionsContainer.js
    - add section
    - delete section
    - modify section headers
    - add buttons for each sections

--- client/src/pages/Assessment/QuestionsContainer.js
+++ client/src/pages/Assessment/QuestionsContainer.js
@@ -11,6 +11,8 @@ import Button from "../../components/Button";
 import * as configStyles from "../../config/styles";
 import CustomRow from "../../components/GridComponents/CustomRow";
 
+import * as MdIcons from "react-icons/md";
+
 class QuestionsContainer extends Component {
   constructor() {
     super();
@@ -25,26 +27,26 @@ class QuestionsContainer extends Component {
             questionChoice: ["B", "C", "A", "D"],
             score: 2,
           },
-          {
-            questionType: "Multiple Choice",
-            questionDescriptive: "adadsadsadsadsaaaaaaaaaaaaa",
-            questionAns: ["A", "B"],
-            questionChoice: ["B", "C", "A", "D"],
-            score: 2,
-          },
-          {
-            questionType: "Descriptive",
-            questionDescriptive:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id tortor porta, faucibus ante a, lacinia leo. Nullam ullamcorper metus id dapibus euismod. Vivamus fermentum pulvinar metus eu ornare. Sed congue ultricies felis in porta. Nullam gravida malesuada neque sit amet euismod. Nulla vehicula dui massa, eu rutrum tortor porta ut. Nulla eu ex ultrices, interdum ex a, consectetur quam. Etiam velit ipsum, eleifend eget mattis vitae, ullamcorper eget dui. Nam id placerat augue, in egestas mi. Praesent mattis tempus interdum. Quisque in lacus dictum, consectetur quam eu, dapibus justo. Phasellus vitae nulla a orci vestibulum lacinia. Vestibulum placerat ut augue a lobortis. Suspendisse elementum porta quam, ac vehicula augue sodales consectetur. In sit amet tortor ac tortor feugiat tincidunt. Proin sit amet vehicula elit. Quisque aliquet orci a accumsan suscipit. Nulla ipsum mauris, volutpat quis ultrices iaculis, sollicitudin sit amet turpis. Nulla malesuada erat nisi, sed ultricies lorem vehicula accumsan.",
-            score: 10,
-          },
-          {
-            questionType: "True or False",
-            questionDescriptive: "1+1=2",
-            questionAns: true,
-            questionChoice: [true, false],
-            score: 2,
-          },
+          // {
+          //   questionType: "Multiple Choice",
+          //   questionDescriptive: "adadsadsadsadsaaaaaaaaaaaaa",
+          //   questionAns: ["A", "B"],
+          //   questionChoice: ["B", "C", "A", "D"],
+          //   score: 2,
+          // },
+          // {
+          //   questionType: "Descriptive",
+          //   questionDescriptive:
+          //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id tortor porta, faucibus ante a, lacinia leo. Nullam ullamcorper metus id dapibus euismod. Vivamus fermentum pulvinar metus eu ornare. Sed congue ultricies felis in porta. Nullam gravida malesuada neque sit amet euismod. Nulla vehicula dui massa, eu rutrum tortor porta ut. Nulla eu ex ultrices, interdum ex a, consectetur quam. Etiam velit ipsum, eleifend eget mattis vitae, ullamcorper eget dui. Nam id placerat augue, in egestas mi. Praesent mattis tempus interdum. Quisque in lacus dictum, consectetur quam eu, dapibus justo. Phasellus vitae nulla a orci vestibulum lacinia. Vestibulum placerat ut augue a lobortis. Suspendisse elementum porta quam, ac vehicula augue sodales consectetur. In sit amet tortor ac tortor feugiat tincidunt. Proin sit amet vehicula elit. Quisque aliquet orci a accumsan suscipit. Nulla ipsum mauris, volutpat quis ultrices iaculis, sollicitudin sit amet turpis. Nulla malesuada erat nisi, sed ultricies lorem vehicula accumsan.",
+          //   score: 10,
+          // },
+          // {
+          //   questionType: "True or False",
+          //   questionDescriptive: "1+1=2",
+          //   questionAns: true,
+          //   questionChoice: [true, false],
+          //   score: 2,
+          // },
         ],
         [
           {
@@ -55,29 +57,29 @@ class QuestionsContainer extends Component {
             questionChoice: ["B", "C", "A", "D"],
             score: 2,
           },
-          {
-            questionType: "Multiple Choice",
-            questionDescriptive: "adadsadsadsadsaaaaaaaaaaaaa",
-            questionAns: ["A", "B"],
-            questionChoice: ["B", "C", "A", "D"],
-            score: 2,
-          },
-          {
-            questionType: "Single Choice",
-            questionDescriptive:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            questionAns: "A",
-            questionChoice: ["B", "C", "A", "D"],
-            score: 2,
-          },
-          {
-            questionType: "Single Choice",
-            questionDescriptive:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            questionAns: "A",
-            questionChoice: ["B", "C", "A", "D"],
-            score: 2,
-          },
+          // {
+          //   questionType: "Multiple Choice",
+          //   questionDescriptive: "adadsadsadsadsaaaaaaaaaaaaa",
+          //   questionAns: ["A", "B"],
+          //   questionChoice: ["B", "C", "A", "D"],
+          //   score: 2,
+          // },
+          // {
+          //   questionType: "Single Choice",
+          //   questionDescriptive:
+          //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          //   questionAns: "A",
+          //   questionChoice: ["B", "C", "A", "D"],
+          //   score: 2,
+          // },
+          // {
+          //   questionType: "Single Choice",
+          //   questionDescriptive:
+          //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          //   questionAns: "A",
+          //   questionChoice: ["B", "C", "A", "D"],
+          //   score: 2,
+          // },
         ],
       ],
     };
@@ -115,13 +117,21 @@ class QuestionsContainer extends Component {
     }
   };
 
+  deleteSection = (index) => {
+    var array = [...this.state.questions];
+    array.splice(index, 1);
+    this.setState({ questions: array });
+  };
+
   render() {
     const { questions } = this.state;
 
     for (let i = 0; i < questions.length; i++) {
-      questions[i].forEach((questions, index) => {
-        questions.serial = index + 1;
-      });
+      if (questions[i] !== null) {
+        questions[i].forEach((questions, index) => {
+          questions.serial = index + 1;
+        });
+      }
     }
 
     return (
@@ -134,43 +144,86 @@ class QuestionsContainer extends Component {
         >
           Add Section
         </Button>
-        <SecondLabel>Questions</SecondLabel>
+        {questions.length > 1 ? (
+          <SecondLabel>Section 1</SecondLabel>
+        ) : (
+          <SecondLabel>Questions</SecondLabel>
+        )}
         <hr className={css(styles.hr)} />
         <SortableRow
           questions={questions[0]}
           onSortEnd={this.onSortEnd.bind(this, 0)}
           onSectionSortEnd={this.onSectionSortEnd.bind(this)}
         />
-        <CustomRow>
-          <div style={{ marginRight: 10 }}>
-            <Button
-              backgroundColor={configStyles.colors.darkBlue}
-              color={configStyles.colors.white}
-              padding={"8px"}
-            >
-              Create Question
-            </Button>
-          </div>
-          <div>
-            <Button
-              backgroundColor={configStyles.colors.darkBlue}
-              color={configStyles.colors.white}
-              padding={"8px"}
-            >
-              Retrieve from Question Bank
-            </Button>
-          </div>
-        </CustomRow>
-        <div style={{ marginBottom: "25px" }}></div>
+        <div style={{ marginBottom: "25px" }}>
+          <CustomRow>
+            <div style={{ marginRight: 10 }}>
+              <Button
+                backgroundColor={configStyles.colors.darkBlue}
+                color={configStyles.colors.white}
+                padding={"8px"}
+              >
+                Create Question
+              </Button>
+            </div>
+            <div>
+              <Button
+                backgroundColor={configStyles.colors.darkBlue}
+                color={configStyles.colors.white}
+                padding={"8px"}
+              >
+                Retrieve from Question Bank
+              </Button>
+            </div>
+          </CustomRow>
+        </div>
 
         {questions.slice(1).map((item, index) => {
           return (
-            <SortableRow
-              questions={questions[index + 1]}
-              onSortEnd={this.onSortEnd.bind(this, index + 1)}
-              onSectionSortEnd={this.onSectionSortEnd.bind(this)}
-              current={index + 1}
-            />
+            <>
+              <CustomRow>
+                <div className={css(styles.label)}>
+                  <SecondLabel>Section {index + 2}</SecondLabel>
+                </div>
+                <div
+                  className={css(styles.button)}
+                  onClick={() => this.deleteSection(index + 1)}
+                >
+                  <MdIcons.MdDelete size={25} />
+                </div>
+              </CustomRow>
+              <hr className={css(styles.hr)} />
+              {item !== null && (
+                <SortableRow
+                  questions={questions[index + 1]}
+                  onSortEnd={this.onSortEnd.bind(this, index + 1)}
+                  onSectionSortEnd={this.onSectionSortEnd.bind(this)}
+                  current={index + 1}
+                />
+              )}
+              <div style={{ marginBottom: "25px" }}>
+                <CustomRow>
+                  <div style={{ marginRight: 10 }}>
+                    <Button
+                      backgroundColor={configStyles.colors.darkBlue}
+                      color={configStyles.colors.white}
+                      padding={"8px"}
+                    >
+                      Create Question
+                    </Button>
+                  </div>
+                  <div>
+                    <Button
+                      backgroundColor={configStyles.colors.darkBlue}
+                      color={configStyles.colors.white}
+                      padding={"8px"}
+                    >
+                      Retrieve from Question Bank
+                    </Button>
+                  </div>
+                </CustomRow>
+              </div>
+            </>
           );
         })}
 
@@ -183,6 +236,27 @@ class QuestionsContainer extends Component {
 const styles = StyleSheet.create({
   hr: {
     backgroundColor: configStyles.colors.black,
+    height: "1px",
+  },
+  button: {
+    color: configStyles.colors.darkBlue,
+    cursor: "pointer",
+    height: "auto",
+    ":active": {
+      color: configStyles.colors.red,
+    },
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    marginLeft: "10px",
+    width: "10%",
+  },
+  label: {
+    height: "auto",
+    width: "90%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    display: "flex",
   },
 });
 
