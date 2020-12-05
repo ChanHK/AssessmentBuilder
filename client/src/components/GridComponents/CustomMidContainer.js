4c944e689ddc1d17260217fbd8a61833d3675c44
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-12-05 15:26:37 +0800

    - check mobile and desktop width

--- client/src/components/GridComponents/CustomMidContainer.js
+++ client/src/components/GridComponents/CustomMidContainer.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { StyleSheet, css } from "aphrodite";
+import { isMobile } from "react-device-detect";
 
 const CustomMidContainer = (props) => (
-  <div className={css(styles.style, props.style)}>{props.children}</div>
+  <div
+    className={css(styles.style, props.style)}
+    style={{ width: isMobile ? "80%" : "65%" }}
+  >
+    {props.children}
+  </div>
 );
 
 const styles = StyleSheet.create({
   style: {
     justifyContent: "center",
     alignItems: "center",
-    width: "65%",
     height: "auto",
     display: "flex",
   },
