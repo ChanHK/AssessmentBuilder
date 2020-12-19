f2183f45d470ded931ca164df29f889d4bf099a8
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-12-19 11:09:13 +0800

    Update index.js

--- client/src/components/LoaderSpinner/index.js
+++ client/src/components/LoaderSpinner/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-// import * as configStyles from "../../config/styles";
 import { StyleSheet, css } from "aphrodite";
 import Loader from "react-loader-spinner";
 
 const LoaderSpinner = () => {
   window.scrollTo(0, 0);
   document.body.style.overflow = "hidden";
-
   return (
     <div className={css(styles.container)}>
       <Loader type="Circles" color="#00BFFF" height={100} width={100} />
