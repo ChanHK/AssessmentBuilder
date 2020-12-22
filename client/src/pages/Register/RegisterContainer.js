dc2d94aaf0fb792f6bae3c8be56ec728e5b9e11c
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-12-22 16:37:41 +0800

    remove componentwillreceiveprops

--- client/src/pages/Register/RegisterContainer.js
+++ client/src/pages/Register/RegisterContainer.js
@@ -39,17 +39,18 @@ class RegisterContainer extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.auth.isAuthenticated) {
-      this.props.clearErrors();
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.auth.isAuthenticated) {
       this.props.history.push("/home");
     }
 
-    if (nextProps.errors) {
-      this.setState({
-        msg: nextProps.errors.message,
-      });
+    if (prevProps.errors !== this.props.errors) {
+      this.setState({ msg: this.props.errors.message });
     }
+  }
+
+  componentWillUnmount() {
+    this.props.clearErrors();
   }
 
   onChange = (e) => {
