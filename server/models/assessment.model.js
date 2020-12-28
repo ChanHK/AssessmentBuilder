2a1fc65498e2ef28b9b99599a33cf96d6f9aea7f
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-12-28 13:38:07 +0800

    Update assessment.model.js

--- server/models/assessment.model.js
+++ server/models/assessment.model.js
@@ -5,6 +5,44 @@ const AssessmentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+  assessments: [
+    {
+      testName: {
+        type: String,
+        default: "New test",
+      },
+      testDescription: {
+        type: String,
+        default: "",
+      },
+      testInstruction: {
+        type: String,
+        default: "",
+      },
+      passOrFailSelected: {
+        type: Boolean,
+        default: true,
+      },
+      score: {
+        type: Number,
+        default: "",
+      },
+      unit: {
+        type: String,
+        default: "",
+      },
+      addGradingSelected: {
+        type: Boolean,
+        default: false,
+      },
+      gradeUnit: {
+        type: String,
+        default: "",
+      },
+      gradeRange: [String],
+      gradeValue: [String],
+    },
+  ],
 });
 
 module.exports = mongoose.model("Assessment", AssessmentSchema);
