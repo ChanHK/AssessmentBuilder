7714a5cb705a33271192f3073f26e688ddd6c6c1
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-12-28 12:15:59 +0800

    Update index.js

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -64,7 +64,7 @@ export default function Routes() {
           isPrivate
         />
         <Route
-          path="/questionbank/question/:type/:questionID"
+          path="/questionbank/question/:type(edit|create)/:questionID"
           exact
           component={CreateQuestionContainer}
           isPrivate
