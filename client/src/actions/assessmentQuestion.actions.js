15085971e0c823edf5801a759d47e51b31e06f7a
ChanHK <59449120+ChanHK@users.noreply.github.com>
2020-12-31 18:46:43 +0800

    redux for questioncontainer

--- client/src/actions/assessmentQuestion.actions.js
+++ client/src/actions/assessmentQuestion.actions.js
@@ -4,7 +4,7 @@ import { ASSESSMENT_QUESTION } from "../utils/actionTypes";
 
 export const addAssessmentQuestion = (data) => (dispatch, getState) => {
   dispatch({ type: ASSESSMENT_QUESTION.ADD_ASSESSMENT_QUESTION_BEGIN });
-  console.log(data);
+
   const tokenConfig = (getState) => {
     const token = getState().auth.token;
     const config = {
@@ -36,5 +36,43 @@ export const addAssessmentQuestion = (data) => (dispatch, getState) => {
       console.log("Add question to assessment failed", err);
       //   dispatch(returnErrors(res.data, res.status));
       dispatch({ type: ASSESSMENT_QUESTION.ADD_ASSESSMENT_QUESTION_FAIL });
+    });
+};
+
+export const fetchAllAssessmentQuestion = (data) => (dispatch, getState) => {
+  dispatch({ type: ASSESSMENT_QUESTION.FETCH_ALL_ASSESSMENT_QUESTION_BEGIN });
+
+  const tokenConfig = (getState) => {
+    const token = getState().auth.token;
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    if (token) config.headers["x-auth-token"] = token;
+
+    return config;
+  };
+
+  axios
+    .get(
+      `/api/user/assessment/question_bank/${data.assessmentID}`,
+      tokenConfig(getState)
+    )
+    .then((res) => {
+      setTimeout(() => {
+        dispatch({
+          type: ASSESSMENT_QUESTION.FETCH_ALL_ASSESSMENT_QUESTION_SUCCESS,
+          payload: res.data,
+        });
+      }, 3000);
+    })
+    .catch((err) => {
+      console.log("Fetch question to assessment failed", err);
+      //   dispatch(returnErrors(res.data, res.status));
+      dispatch({
+        type: ASSESSMENT_QUESTION.FETCH_ALL_ASSESSMENT_QUESTION_FAIL,
+      });
     });
 };
--- client/src/reducers/assessmentQuestion.reducer.js
+++ client/src/reducers/assessmentQuestion.reducer.js
@@ -8,17 +8,20 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case ASSESSMENT_QUESTION.ADD_ASSESSMENT_QUESTION_BEGIN:
+    case ASSESSMENT_QUESTION.FETCH_ALL_ASSESSMENT_QUESTION_BEGIN:
       return {
         ...state,
         isLoading: true,
       };
     case ASSESSMENT_QUESTION.ADD_ASSESSMENT_QUESTION_SUCCESS:
+    case ASSESSMENT_QUESTION.FETCH_ALL_ASSESSMENT_QUESTION_SUCCESS:
       return {
         ...state,
         isLoading: false,
         assessmentLoad: action.payload,
       };
     case ASSESSMENT_QUESTION.ADD_ASSESSMENT_QUESTION_FAIL:
+    case ASSESSMENT_QUESTION.FETCH_ALL_ASSESSMENT_QUESTION_FAIL:
       return {
         ...state,
         isLoading: false,
