d61043216ce059dbfaecb336cdd27a6a0fc20883
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-02 14:50:52 +0800

    update

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -123,7 +123,7 @@ export default function Routes() {
           isPrivate
         />
         <Route
-          path="/assessment/update_question/:section/:type(edit|create)/:assessmentID/:questionID"
+          path="/assessment/update_question/:section/:type(edit|create)/:type2(edit|create)/:assessmentID/:questionID"
           exact
           component={CreateEditQuestionContainer}
           isPrivate
