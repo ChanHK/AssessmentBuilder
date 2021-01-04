586ae8fba2e9bf01b28ded0e36d9e6d0bdb65972
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-04 21:17:53 +0800

    Update index.js

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -14,7 +14,6 @@ const Modal = (props) => (
 const styles = StyleSheet.create({
   modal: {
     backgroundColor: "rgba(20,20,20,0.9)", //#333333
-    zIndex: 10,
     width: "100%",
     height: "100%",
     position: "fixed",
