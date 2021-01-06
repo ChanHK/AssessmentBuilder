dcc948a152430015ad2603d425629769675f8d59
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-06 21:49:22 +0800

    backforth submit button base on array

--- client/src/pages/Candidate/AttemptContainer.js
+++ client/src/pages/Candidate/AttemptContainer.js
@@ -2,6 +2,147 @@ import React, { Component } from "react";
 import { StyleSheet, css } from "aphrodite";
 import * as configStyles from "../../config/styles";
 
-class AttemptContainer extends Component {}
+import CustomFullContainer from "../../components/GridComponents/CustomFullContainer";
+import CustomMidContainer from "../../components/GridComponents/CustomMidContainer";
+import CustomColumn from "../../components/GridComponents/CustomColumn";
+import CustomRow from "../../components/GridComponents/CustomRow";
+
+import CustomSubLabel from "../../components/FormComponents/CustomSubLabel";
+
+import ScrollArrow from "../../components/ScrollArrow";
+import CustomEditor from "../../components/CustomEditor";
+import Button from "../../components/Button";
+
+import { EditorState, convertToRaw, ContentState } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import htmlToDraft from "html-to-draftjs";
+
+class AttemptContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      index: 0, //array index
+      question: [],
+    };
+  }
+
+  convertHtml = (data) => {
+    const contentBlock = htmlToDraft(data);
+    if (contentBlock) {
+      const contentState = ContentState.createFromBlockArray(
+        contentBlock.contentBlocks
+      );
+      return EditorState.createWithContent(contentState);
+    }
+  };
+
+  back = () => {
+    const { index } = this.state;
+    if (index > 0) {
+      this.setState({ index: index - 1 });
+    }
+  };
+
+  next = () => {
+    const { index, question } = this.state;
+    if (index < question.length - 1) {
+      this.setState({ index: index + 1 });
+    }
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
+    const { question, index } = this.state;
+
+    return (
+      <>
+        <ScrollArrow />
+        <CustomFullContainer>
+          <CustomMidContainer>
+            <form onSubmit={this.onSubmit} className={css(styles.con)}>
+              <CustomColumn>
+                <div style={{ marginTop: "25px" }}>
+                  <h4 className={css(styles.questionText)}>
+                    Question {index + 1}
+                  </h4>
+                </div>
+
+                <div style={{ marginBottom: "25px", marginTop: "15px" }}>
+                  <CustomEditor
+                    toolbarHidden={true}
+                    readOnly={true}
+                    heightAuto={true}
+                    editorState={this.convertHtml(
+                      question[index].questionDescription
+                    )}
+                  />
+                </div>
+
+                <div className={css(styles.buttonRowCon)}>
+                  <CustomRow>
+                    <div style={{ marginRight: "15px" }}>
+                      <Button
+                        backgroundColor={configStyles.colors.darkBlue}
+                        color={configStyles.colors.white}
+                        padding={"8px"}
+                        width={"75px"}
+                        type={"button"}
+                        onClick={this.back}
+                      >
+                        Back
+                      </Button>
+                    </div>
+                    <div style={{ marginRight: "15px" }}>
+                      <Button
+                        backgroundColor={configStyles.colors.darkBlue}
+                        color={configStyles.colors.white}
+                        padding={"8px"}
+                        width={"75px"}
+                        type={"button"}
+                        onClick={this.next}
+                      >
+                        Next
+                      </Button>
+                    </div>
+                    <div>
+                      {question.length - 1 === index && (
+                        <Button
+                          backgroundColor={configStyles.colors.darkBlue}
+                          color={configStyles.colors.white}
+                          padding={"8px"}
+                          width={"75px"}
+                          type={"submit"}
+                        >
+                          Submit
+                        </Button>
+                      )}
+                    </div>
+                  </CustomRow>
+                </div>
+              </CustomColumn>
+            </form>
+          </CustomMidContainer>
+        </CustomFullContainer>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  con: {
+    width: "100%",
+  },
+  questionText: {
+    fontFamily: "Ubuntu-bold",
+    fontColor: configStyles.colors.black,
+  },
+  buttonRowCon: {
+    width: "100%",
+    display: "flex",
+  },
+});
 
 export default AttemptContainer;
