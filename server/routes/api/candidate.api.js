7f275b9eac38257d066dd310a0e2d9b0a4490357
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-07 23:23:10 +0800

    candidtae register API (with accessCode)

--- server/routes/api/candidate.api.js
+++ server/routes/api/candidate.api.js
@@ -24,9 +24,63 @@ router.get("/start/assessment/fetch/:assessmentID", (req, res) => {
     .catch((err) => console.log(err));
 });
 
-// @route     POST api/auth/candidate/join
+// @route     POST api/candidate/start/assessment/register/with_auth/:assessmentID
 // @desc      register user and return JWT token
 // @access    Public
-// router.post("/start/assessment/register/:assessmentID");
+router.post(
+  "/start/assessment/register/with_auth/:assessmentID",
+  (req, res) => {
+    db.Assessment.findOne({
+      assessments: { $elemMatch: { _id: req.params.assessmentID } },
+    })
+      .then((array) => {
+        let i = 0;
+        array.assessments.forEach((item, index) => {
+          if (
+            JSON.stringify(item._id) ===
+            `"` + req.params.assessmentID + `"`
+          ) {
+            i = index;
+          }
+        });
+        let temp = false;
+        array.assessments[i].access.accessEmail.forEach((item, index) => {
+          if (
+            item === req.body.email &&
+            array.assessments[i].access.accessCode[index] ===
+              req.body.accessCode
+          ) {
+            temp = true;
+            const newCand = {
+              assessments_id: req.params.assessmentID,
+              name: req.body.name,
+              email: req.body.email,
+            };
+            db.Candidate.create(newCand)
+              .then((Cand) => {
+                jwt.sign(
+                  { id: Cand.id },
+                  process.env.JWT_TOKEN_KEY_CAND,
+                  {
+                    expiresIn: 86400, //1 day
+                  },
+                  (err, token) => {
+                    if (err) throw err;
+                    res.json({
+                      role: "Candidate",
+                      token: token,
+                    });
+                  }
+                );
+              })
+              .catch((err) => console.log(err));
+          }
+        });
+        if (!temp)
+          return res.status(400).json({ message: "Invalid credentials" });
+      })
+      .catch((err) => console.log(err));
+  }
+);
 
 module.exports = router;
