b4fbce4be9fdcc62a7963f0cccb2776219168698
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-07 22:28:36 +0800

    Create candidate.api.js

---
+++ server/routes/api/candidate.api.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
+
+const db = require("../../models");
+
+// @route     GET api/candidate/start/assessment/fetch/:assessmentID
+// @desc      GET assessment name and instructions
+// @access    Private
+router.get("/start/assessment/fetch/:assessmentID", auth, (req, res) => {
+  db.Assessment.findOne({
+    assessments: { $elemMatch: { _id: req.params.assessmentID } },
+  })
+    .then((array) => {
+      let i = 0;
+      array.assessments.forEach((item, index) => {
+        if (JSON.stringify(item._id) === `"` + req.params.assessmentID + `"`) {
+          i = index;
+        }
+      });
+      return res.json(array.assessments[i].settings);
+    })
+    .catch((err) => console.log(err));
+});
+
+// @route     POST api/auth/candidate/join
+// @desc      register user and return JWT token
+// @access    Public
+
+module.exports = router;
