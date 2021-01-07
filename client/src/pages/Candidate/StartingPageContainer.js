e4303d6c091ba3fe9d5f7c8af7b39c4b9c8b1e04
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-07 22:28:44 +0800

    Update StartingPageContainer.js

--- client/src/pages/Candidate/StartingPageContainer.js
+++ client/src/pages/Candidate/StartingPageContainer.js
@@ -12,11 +12,17 @@ import ScrollArrow from "../../components/ScrollArrow";
 import CustomEditor from "../../components/CustomEditor";
 import CustomInput from "../../components/CustomInput";
 import Button from "../../components/Button";
+import LoaderSpinner from "../../components/LoaderSpinner";
 
-// import { EditorState, convertToRaw, ContentState } from "draft-js";
-import { EditorState } from "draft-js";
-// import draftToHtml from "draftjs-to-html";
-// import htmlToDraft from "html-to-draftjs";
+import { EditorState, ContentState } from "draft-js";
+import htmlToDraft from "html-to-draftjs";
+
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchAssessmentInfo } from "../../actions/candidate.actions";
+
+import jwt_decode from "jwt-decode";
+import { logout } from "../../actions/auth.actions";
 
 class StartingPageContainer extends Component {
   constructor() {
@@ -29,6 +35,60 @@ class StartingPageContainer extends Component {
       accessCode: "",
     };
   }
+
+  componentDidMount() {
+    if (localStorage.getItem("token")) {
+      const token = localStorage.getItem("token");
+      const decoded = jwt_decode(token);
+
+      // Check for expired token
+      const currentTime = Date.now() / 1000; // to get in milliseconds
+      if (decoded.exp < currentTime) {
+        this.props.logout();
+        this.props.history.push("/login");
+      }
+    }
+    const data = {
+      assessmentID: this.props.match.params.assessmentID,
+    };
+
+    this.props.fetchAssessmentInfo(data);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { candidateReducer } = this.props;
+
+    if (
+      prevProps.candidateReducer !== candidateReducer &&
+      candidateReducer.assessmentStartInfo !== null
+    ) {
+      const {
+        testName,
+        testInstruction,
+      } = candidateReducer.assessmentStartInfo;
+
+      let temp = this.convert(testInstruction);
+
+      this.setState({
+        assessmentTitle: testName,
+        instruction: temp,
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.candidateReducer.assessmentStartInfo = null;
+  }
+
+  convert = (data) => {
+    const contentBlock = htmlToDraft(data);
+    if (contentBlock) {
+      const contentState = ContentState.createFromBlockArray(
+        contentBlock.contentBlocks
+      );
+      return EditorState.createWithContent(contentState);
+    }
+  };
 
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
@@ -46,6 +106,9 @@ class StartingPageContainer extends Component {
       email,
       accessCode,
     } = this.state;
+
+    if (this.props.candidateReducer.isLoading) return <LoaderSpinner />;
+    else document.body.style.overflow = "unset";
 
     return (
       <>
@@ -141,4 +204,16 @@ const styles = StyleSheet.create({
   },
 });
 
-export default StartingPageContainer;
+StartingPageContainer.propTypes = {
+  fetchAssessmentInfo: PropTypes.func.isRequired,
+  candidateReducer: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  candidateReducer: state.candidateReducer,
+});
+
+export default connect(mapStateToProps, { fetchAssessmentInfo, logout })(
+  StartingPageContainer
+);
