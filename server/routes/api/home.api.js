918e0a5b04bd80a4e6500e63e82e7d22f1f0472f
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-11 21:28:56 +0800

    Update home.api.js

--- server/routes/api/home.api.js
+++ server/routes/api/home.api.js
@@ -29,4 +29,51 @@ router.get("/assessment/fetch", auth, (req, res) => {
     .catch((err) => console.log(err));
 });
 
+// @route     POST api/user/home/assessment/create
+// @desc      POST create new assessment obj
+// @access    Private
+router.post("/assessment/create", auth, (req, res) => {
+  db.Assessment.findOne({ user_id: req.user.id })
+    .then((assessment) => {
+      db.Assessment.findByIdAndUpdate(
+        assessment._id,
+        {
+          $push: {
+            assessments: {
+              settings: {
+                testName: "New assessment",
+              },
+            },
+          },
+        },
+        { new: true }
+      )
+        .then((assessment) => {
+          const data = {
+            assessments_id:
+              assessment.assessments[assessment.assessments.length - 1]._id,
+          };
+
+          db.AssessmentQuestion.create(data);
+          db.AssessmentSet.create(data);
+          db.User.updateOne(
+            { _id: req.user.id },
+            {
+              $inc: { totalAssessmentsCreated: 1 },
+            }
+          );
+          return res.json({
+            assessmentID:
+              assessment.assessments[assessment.assessments.length - 1]._id,
+          });
+        })
+        .catch((err) => {
+          return res.status(400).json({
+            message: "Error, failed to create assessment, please retry agian",
+          });
+        });
+    })
+    .catch((err) => console.log(err));
+});
+
 module.exports = router;
