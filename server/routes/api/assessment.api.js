c0a9dbcfc833cb71846a93b2ec596ed61fb9744f
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-11 21:04:45 +0800

    Update assessment.api.js

--- server/routes/api/assessment.api.js
+++ server/routes/api/assessment.api.js
@@ -4,53 +4,6 @@ const auth = require("../../middleware/auth");
 
 const db = require("../../models");
 
-// @route     POST api/user/assessment/create
-// @desc      POST create new assessment obj
-// @access    Private
-router.post("/assessment/create", auth, (req, res) => {
-  db.Assessment.findOne({ user_id: req.user.id })
-    .then((assessment) => {
-      db.Assessment.findByIdAndUpdate(
-        assessment._id,
-        {
-          $push: {
-            assessments: {
-              settings: {
-                testName: "New assessment",
-              },
-            },
-          },
-        },
-        { new: true }
-      )
-        .then((assessment) => {
-          const data = {
-            assessments_id:
-              assessment.assessments[assessment.assessments.length - 1]._id,
-          };
-
-          db.AssessmentQuestion.create(data);
-          db.AssessmentSet.create(data);
-          db.User.updateOne(
-            { _id: req.user.id },
-            {
-              $inc: { totalAssessmentsCreated: 1 },
-            }
-          );
-          return res.json({
-            assessmentID:
-              assessment.assessments[assessment.assessments.length - 1]._id,
-          });
-        })
-        .catch((err) => {
-          return res.status(400).json({
-            message: "Error, failed to create assessment, please retry agian",
-          });
-        });
-    })
-    .catch((err) => console.log(err));
-});
-
 // @route     POST api/user/assessment/settings/update/:assessmentID
 // @desc      POST settings to assessment collection
 // @access    Private
