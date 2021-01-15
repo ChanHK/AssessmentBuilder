db8073a973ff48afbc429a061de10f5512d381bb
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-15 22:51:39 +0800

    Update ViewResponseContainer.js

--- client/src/pages/Home/ViewResponseContainer.js
+++ client/src/pages/Home/ViewResponseContainer.js
@@ -5,6 +5,7 @@ import * as configStyles from "../../config/styles";
 
 import Header from "../../components/Header";
 import ScrollArrow from "../../components/ScrollArrow";
+import LoaderSpinner from "../../components/LoaderSpinner";
 
 import CustomFullContainer from "../../components/GridComponents/CustomFullContainer";
 import CustomMidContainer from "../../components/GridComponents/CustomMidContainer";
@@ -15,8 +16,59 @@ import FirstLabel from "../../components/LabelComponent/FirstLabel";
 import SecondLabel from "../../components/LabelComponent/SecondLabel";
 import ThirdLabel from "../../components/LabelComponent/ThirdLabel";
 
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchAResult } from "../../actions/home.actions";
+import jwt_decode from "jwt-decode";
+import { logout } from "../../actions/auth.actions";
+
 class ViewResponseContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      candID: this.props.match.params.candID,
+    };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem("token")) {
+      const token = localStorage.getItem("token");
+      const decoded = jwt_decode(token);
+
+      // Check for expired token
+      const currentTime = Date.now() / 1000; // to get in milliseconds
+      if (decoded.exp < currentTime) {
+        this.props.logout();
+        this.props.history.push("/login");
+      }
+    }
+
+    const data = {
+      candID: this.state.candID,
+    };
+
+    this.props.fetchAResult(data);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { homeReducer } = this.props;
+
+    if (
+      prevProps.homeReducer !== homeReducer &&
+      homeReducer.aCandResult !== null
+    ) {
+      console.log(homeReducer.aCandResult);
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.homeReducer.aCandResult = null;
+  }
+
   render() {
+    if (this.props.homeReducer.isLoading) return <LoaderSpinner />;
+    else document.body.style.overflow = "unset";
+
     return (
       <>
         <Header />
@@ -90,4 +142,17 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ViewResponseContainer;
+ViewResponseContainer.propTypes = {
+  fetchAResult: PropTypes.func.isRequired,
+  homeReducer: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  homeReducer: state.homeReducer,
+});
+
+export default connect(mapStateToProps, {
+  fetchAResult,
+  logout,
+})(ViewResponseContainer);
