d44edf62c8f961b98da27b56b2d0ed957a113961
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-01-21 15:41:22 +0800

    Update profile.actions.js

--- client/src/actions/profile.actions.js
+++ client/src/actions/profile.actions.js
@@ -5,20 +5,21 @@ import { PROFILE_DATA } from "../utils/actionTypes";
 // import { returnErrors } from "./error.actions";
 import { returnSucMsg } from "./sucMsg.actions";
 
+const tokenConfig = (getState) => {
+  const token = getState().auth.token;
+  const config = {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  };
+
+  if (token) config.headers["x-auth-token"] = token;
+
+  return config;
+};
+
 export const fetchUserProfileData = () => (dispatch, getState) => {
   dispatch({ type: PROFILE_DATA.FETCH_BEGIN });
-
-  const tokenConfig = (getState) => {
-    const token = getState().auth.token;
-    const config = {
-      headers: {
-        "Content-type": "application/json",
-      },
-    };
-    if (token) config.headers["x-auth-token"] = token;
-
-    return config;
-  };
 
   axios
     .get("/api/user/profile", tokenConfig(getState))
@@ -35,19 +36,6 @@ export const fetchUserProfileData = () => (dispatch, getState) => {
 
 export const updateUserProfileData = (data) => (dispatch, getState) => {
   dispatch({ type: PROFILE_DATA.UPDATE_PROFILE_DATA_BEGIN });
-
-  const tokenConfig = (getState) => {
-    const token = getState().auth.token;
-    const config = {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    };
-
-    if (token) config.headers["x-auth-token"] = token;
-
-    return config;
-  };
 
   axios
     .post("/api/user/profile", data, tokenConfig(getState))
