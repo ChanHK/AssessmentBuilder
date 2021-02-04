fc09b1f572effd95fc0f3504e1f5683556db5df7
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-02-04 21:37:06 +0800

    heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const path = require("path");
+const path = require("path");
 
 //env config # for heroku deployment
 require("dotenv").config({
@@ -45,14 +45,18 @@ app.use("/api/candidate", candidate);
 app.use("/api/user/home", home);
 app.use("/api/user/home2", home2);
 
-// app.use(express.static(path.join(__dirname, "../client/build")));
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-// });
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
