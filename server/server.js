c986529507cf11c0065d614e654119e83fb2ba93
ChanHK <59449120+ChanHK@users.noreply.github.com>
2021-02-04 20:48:16 +0800

    update

--- server/server.js
+++ server/server.js
@@ -55,3 +55,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.get("/", (req, res) => res.send("Hello from Express!"));
